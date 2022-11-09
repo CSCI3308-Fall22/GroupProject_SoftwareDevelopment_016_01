46682e648c83df5a3ebd82484992c6885f2a5116
ZakBasso <zaba5885@colorado.edu>
2022-11-09 16:41:11 -0700

    test data for programs, and events

--- ProjectCode/init_data/create.sql
+++ ProjectCode/init_data/create.sql
@@ -7,6 +7,7 @@ CREATE TABLE users(
 DROP TABLE IF EXISTS programs CASCADE;
 CREATE TABLE programs(
     program_id VARCHAR(50) PRIMARY KEY,
+    program_name VARCHAR(100),
     password CHAR(60),
     owner_name VARCHAR(75)
 );
@@ -31,4 +32,15 @@ CREATE TABLE events(
 
 
 
+-- Test Data --
+--Users
 INSERT INTO users (username, password) VALUES ("TestUser1", "TestPassword1"),("TestUser2", "TestPassword2");
+
+--programs
+INSERT INTO programs (program_id, program_name, password, owner_name) VALUES ("1","Fall Broomball 2022","","owner1"),("2","var soccer 2022","pass2","owner2"),("3","football i guess","","owner3");
+
+--usersToPrograms
+INSERT INTO usersToPrograms (username, program_id) VALUES ("TestUser1","1"),("TestUser1","2"),("TestUser2","2"),("TestUser2","3");
+
+--events
+INSERT INTO events (event_id, program_id, title, "day", "time", description) VALUES ("1","1","Game","tuesday","01:50","Be there 15 minutes early. CU rec-center ice rink");
