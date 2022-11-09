e60fbe5f2023242440a95580a2484435ddd02a3e
ZakBasso <zaba5885@colorado.edu>
2022-11-09 15:50:32 -0700

    database expansion

--- ProjectCode/init_data/create.sql
+++ ProjectCode/init_data/create.sql
@@ -7,8 +7,8 @@ CREATE TABLE users(
 DROP TABLE IF EXISTS programs CASCADE;
 CREATE TABLE programs(
     program_id VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
-
+    password CHAR(60),
+    owner_name VARCHAR(75)
 );
 
 DROP TABLE IF EXISTS usersToPrograms CASCADE;
@@ -16,3 +16,16 @@ CREATE TABLE usersToPrograms(
     username VARCHAR(50),
     program_id CHAR(60)
 );
+
+CREATE TYPE day AS ENUM ('sunday', 'monday', 'tuesday', 'thursday', 'friday', 'saturday');
+
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE events(
+    event_id VARCHAR(50) PRIMARY KEY,
+    program_id CHAR(60),
+    title VARCHAR(100),
+    day day,
+    time time,
+    description VARCHAR(1000)
+);
+
