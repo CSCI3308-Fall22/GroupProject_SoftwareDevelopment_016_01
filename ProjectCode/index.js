f6e8ebaf93a8861964e780d76714e459f620099e
William Weber <wiwe4856@colorado.edu>
2022-11-29 22:13:43 -0700

    A lot of changes, good progress

--- ProjectCode/index.js
+++ ProjectCode/index.js
@@ -69,7 +69,7 @@ app.post('/register', async (req, res) => {
             res.locals.error = "danger";
             res.render("pages/register.ejs");
             //res.redirect('/register')
-            
+
         });
 });
 
@@ -89,8 +89,8 @@ app.post('/login', async (req, res) => {
                 res.redirect('/programs')
             }
         })
-        .catch(() => {
-            res.locals.message = "Username/Password Incorrect";
+        .catch((err) => {
+            res.locals.message = err;
             res.locals.error = "danger";
             res.render("pages/login.ejs");
         });
@@ -116,13 +116,15 @@ app.get('/logout', (req, res) => {
 
 app.get('/programs', (req, res) => {
     const username_ = req.session.user.username;
-    db.any("SELECT * FROM programs JOIN usersToPrograms ON programs.program_id = usersToPrograms.program_id WHERE usersToPrograms.username = $1", [username_] )
+    db.any("SELECT * FROM programs JOIN usersToPrograms ON programs.program_id = usersToPrograms.program_id WHERE usersToPrograms.username = $1", [username_])
         .then((data) => {
-            console.log("programs",data)
-            res.render("pages/programs.ejs", {data:data})
+            console.log("programs")//,data)
+            res.render("pages/programs.ejs", { data: data })
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
         });
 });
 
@@ -130,20 +132,22 @@ app.get('/joinprograms', (req, res) => {
     const username_ = req.session.user.username;
     db.any("SELECT programs.program_id, program_name, password, owner_name FROM programs EXCEPT SELECT programs.program_id, program_name, password, owner_name FROM programs JOIN usersToPrograms ON programs.program_id = usersToPrograms.program_id WHERE usersToPrograms.username = $1", [username_])
         .then((data) => {
-            console.log("Join programs",data)
-            res.render("pages/joinprograms.ejs", {data:data})
+            console.log("Join programs", data)
+            res.render("pages/joinprograms.ejs", { data: data })
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
         });
 });
 
 app.get('/calendar', (req, res) => {
     const username_ = req.session.user.username;
-    db.any("SELECT * FROM events JOIN (SELECT programs.program_id FROM programs JOIN usersToPrograms ON programs.program_id = usersToPrograms.program_id WHERE usersToPrograms.username = $1) AS x  ON x.program_id = events.program_id", [username_])
+    db.any("SELECT * FROM events JOIN (SELECT programs.program_id FROM programs JOIN usersToPrograms ON programs.program_id = usersToPrograms.program_id WHERE usersToPrograms.username = $1) AS x  ON x.program_id = events.program_id ORDER BY events.time", [username_])
         .then((data) => {
-            console.log(data)
-            res.render("pages/calendar.ejs", {data:data})
+            console.log("calender", data)
+            res.render("pages/calendar.ejs", { data: data })
         })
         .catch((err) => {
             console.log(err)
@@ -153,29 +157,49 @@ app.get('/calendar', (req, res) => {
 app.post('/joinprogram', (req, res) => {
     const program_id = req.body.program_id;
     const username_ = req.session.user.username;
-    db.any("INSERT INTO usersToPrograms (username, program_id) VALUES ($1, $2)", [username_,program_id])
+    db.any("INSERT INTO usersToPrograms (username, program_id) VALUES ($1, $2)", [username_, program_id])
         .then((data) => {
-            console.log("Join programs join",data)
+            console.log("Join programs join", data)
             res.redirect("/joinprograms")
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
+        });
+});
+app.post('/joinprogram/add', (req, res) => {
+    const pName = req.body.program_name;
+    const description = req.body.desc;
+    const year_ = req.body.year;
+    const username_ = req.session.user.username;
+    db.any("INSERT INTO programs (program_name, description, owner_name, year) VALUES ($1, $2, $3, $4)", [pName, description, username_, year_])
+        .then((data) => {
+            console.log("Join programs join", data)
+            res.redirect("/joinprograms")
+        })
+        .catch((err) => {
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
         });
 });
 
 app.get('/leaderboard', (req, res) => {
-    db.any("SELECT * FROM PRtable")
+    db.any("SELECT * FROM PRtable ORDER BY ((weightrecord+runrecord)/2) DESC")
         .then((data) => {
             console.log(data)
-            res.render("pages/leaderboard.ejs", {data:data})
+            res.render("pages/leaderboard.ejs", { data: data })
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
         });
 });
 
-app.get('/records', (req,res) => {
-    res.render ('pages/record.ejs');
+app.get('/records', (req, res) => {
+    res.render('pages/record.ejs');
 })
 
 app.post('/prUpdate', (req, res) => {
@@ -183,11 +207,11 @@ app.post('/prUpdate', (req, res) => {
     const weightRec = req.body.weightRecord;
     const runRec = req.body.runRecord;
     const queryPR = "SELECT * FROM PRtable WHERE username = $1";
-    
+
     db.any(queryPR, [username_])
         .then((data) => {
 
-            if (data[0] != null){
+            if (data[0] != null) {
                 query1 = "UPDATE PRtable SET weightRecord = $2, runRecord = $3 WHERE PRtable.username = $1";
             }
             else {
@@ -195,12 +219,41 @@ app.post('/prUpdate', (req, res) => {
             }
 
             db.any(query1, [username_, weightRec, runRec])
-            console.log("PersonalRec",data)
-            res.redirect("/records")
+            res.locals.message = "Added to System!";
+            res.locals.success = "success";
+            console.log("PersonalRec", data)
+            res.redirect("/leaderboard")
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
         });
 });
 
+app.get('/leave', (req, res) => {
+    const username_ = req.session.user.username;
+    db.any('DELETE FROM usersToPrograms WHERE usersToPrograms.username = $1 AND usersToPrograms.program_id = $2', [username_, req.query.program_id])
+        .then(() => {
+            console.log("leave",)
+            res.redirect("/programs")
+        })
+        .catch((err) => {
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
+        });
+});
 
+app.post('/addevent', (req, res) => {
+    db.any('INSERT INTO events (program_id, title, "day", "time", description) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.program_id, req.body.title, req.body.day, req.body.time, req.body.description])
+        .then(() => {
+            console.log("add Event", data)
+            res.redirect("/calender")
+        })
+        .catch((err) => {
+            console.log(err);
+            res.locals.message = err;
+            res.locals.error = "danger";
+        });
+});
