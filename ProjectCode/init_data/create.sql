4646f65e6642c28d04b28c0d8c5f9ca6648c60f9
William Weber <wiwe4856@colorado.edu>
2022-12-01 12:15:59 -0700

    Final push to joey-branch, switching to main branch

--- ProjectCode/init_data/create.sql
+++ ProjectCode/init_data/create.sql
@@ -25,7 +25,7 @@ CREATE TABLE usersToPrograms
     FOREIGN KEY (program_id) REFERENCES programs (program_id)
 );
 
-CREATE TYPE day AS ENUM ('sunday', 'monday', 'tuesday', 'thursday', 'friday', 'saturday');
+CREATE TYPE day AS ENUM ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
 
 DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE events
@@ -33,7 +33,7 @@ CREATE TABLE events
     event_id     SERIAL PRIMARY KEY NOT NULL,
     program_id   INT,
     title        VARCHAR(100),
-    day day,
+    day          day,
     time         time,
     duration_min int,
     description  VARCHAR(1000),
@@ -57,13 +57,19 @@ VALUES ('TestUser1', '$2b$10$2FPeavnxF81f5pnDUkdq7OID7ANcWzC2VugYSECWvcCNaf184rW
 -- TestPassword1, TestPassword2
 
 --programs
-INSERT INTO programs (program_id, program_name, password, owner_name, year, description)
-VALUES (1, 'Fall Broomball', '', 'owner1', 2022,
+INSERT INTO programs (program_name, password, owner_name, year, description)
+VALUES ('Fall Broomball', '', 'YB', 2022,
         'This Fall, join the CU Rec Center on the ice for a competitive but fun season of Broomball! Grab your broom and your skates, we will provide the ice! Bring your BuffOne and a registered team every Thursday night otside the Ice Arena and follow this program for the scheduled workouts. See the Associates at the front desk for details and any questions. See you soon!'),
-       (2, 'Varsity Soccer', 'pass2', 'owner2', 2022,
+       ('Varsity Soccer', 'pass2', 'Zak', 2022,
         'Grab your cleats and jerseys because this fall, CU Rec Center is offering a varsity soccer training program through WeFit! Click the link to join the program and recieve uptates on the calendar with workout plans and meeting times. See you soon!'),
-       (3, 'Indoor Soccer', '', 'owner3', 2022,
-        'Looking to condition before the next season? Joing the Fall Indoor Soccer program to stay fit with WeFit! Programs are posted here for workouts and meting times. These are intense but fun and sponsored by the CU Personal Trainers office!');
+       ('Philadelphia Eagles Football', '', 'Joey', 2022,
+        'Join the Philadelphia Eagles as they tear through this season! There are limited spots for this high caliber team so come find out if you have the grit to be on our team!'),
+        ('Indoor Soccer', '', 'Arnav', 2022,
+        'Looking to condition before the next season? Joing the Fall Indoor Soccer program to stay fit with WeFit! Programs are posted here for workouts and meting times. These are intense but fun and sponsored by the CU Personal Trainers office!'),
+        ('Personal Trainer', '', 'Matt', 2022,
+        'Matt is actually a personal trainer, ask him about it! he just might train you. This is a real plug, you should actually ask him!'),
+        ('CU Boulder Wrestling', '', 'Nikita', 2022,
+        'Nikitas Famous intense wrestling camp, come and find out how tough you are. She will put you through the gauntlet and really test how tough you are!!! She is not responsible for any injuries.');
 
 --usersToPrograms
 INSERT INTO usersToPrograms (username, program_id)
@@ -73,7 +79,9 @@ VALUES ('TestUser1', 1),
        ('TestUser2', 3);
 
 --events
-INSERT INTO events (event_id, program_id, title, "day", "time", description)
-VALUES (1, 1, 'Game', 'tuesday', '01:50', 'Be there 15 minutes early. CU rec-center ice rink'),
-(2, 2, 'Practice', 'monday', '12:30', 'Bring cleats and a water bottle for the sprint workshop. Meet by the indoor arena. '),
-(3, 3, '3 V 3 Tournament', 'tuesday', '3:00', 'Bring cleats and a water bottle. Form teams of three outside the indoor arena while warming up');
+INSERT INTO events (program_id, title, "day", "time", description)
+VALUES (1, 'Game', 'tuesday', '01:50', 'Be there 15 minutes early. CU rec-center ice rink'),
+       (2, 'Practice2', 'thursday', '12:30', 'tle for the sprint workshop. Meet by the indoor arena. '),
+       (3, '3 V 3 Tournament', 'tuesday', '3:00',
+        'Bring cleats and a water bottle. Form teams of three outside the indoor arena while warming up'),
+       (1, 'Practice', 'monday', '12:30', 'Bring cleats and a water bottle for the sprint workr arena. ');
