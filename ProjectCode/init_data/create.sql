96c9c2218a4ea7e917ed6df0a8b148a7d0687768
William Weber <wiwe4856@colorado.edu>
2022-12-01 12:25:50 -0700

    Made join programs examples more personal

--- ProjectCode/init_data/create.sql
+++ ProjectCode/init_data/create.sql
@@ -58,12 +58,18 @@ VALUES ('TestUser1', '$2b$10$2FPeavnxF81f5pnDUkdq7OID7ANcWzC2VugYSECWvcCNaf184rW
 
 --programs
 INSERT INTO programs (program_name, password, owner_name, year, description)
-VALUES ('Fall Broomball', '', 'owner1', 2022,
+VALUES ('Fall Broomball', '', 'YB', 2022,
         'This Fall, join the CU Rec Center on the ice for a competitive but fun season of Broomball! Grab your broom and your skates, we will provide the ice! Bring your BuffOne and a registered team every Thursday night otside the Ice Arena and follow this program for the scheduled workouts. See the Associates at the front desk for details and any questions. See you soon!'),
-       ('Varsity Soccer', 'pass2', 'owner2', 2022,
+       ('Varsity Soccer', 'pass2', 'Zak', 2022,
         'Grab your cleats and jerseys because this fall, CU Rec Center is offering a varsity soccer training program through WeFit! Click the link to join the program and recieve uptates on the calendar with workout plans and meeting times. See you soon!'),
-       ('Indoor Soccer', '', 'owner3', 2022,
-        'Looking to condition before the next season? Joing the Fall Indoor Soccer program to stay fit with WeFit! Programs are posted here for workouts and meting times. These are intense but fun and sponsored by the CU Personal Trainers office!');
+       ('Philadelphia Eagles Football', '', 'Joey', 2022,
+        'Join the Philadelphia Eagles as they tear through this season! There are limited spots for this high caliber team so come find out if you have the grit to be on our team!'),
+        ('Indoor Soccer', '', 'Arnav', 2022,
+        'Looking to condition before the next season? Joing the Fall Indoor Soccer program to stay fit with WeFit! Programs are posted here for workouts and meting times. These are intense but fun and sponsored by the CU Personal Trainers office!'),
+        ('Personal Trainer', '', 'Matt', 2022,
+        'Matt is actually a personal trainer, ask him about it! he just might train you. This is a real plug, you should actually ask him!'),
+        ('CU Boulder Wrestling', '', 'Nikita', 2022,
+        'Nikitas Famous intense wrestling camp, come and find out how tough you are. She will put you through the gauntlet and really test how tough you are!!! She is not responsible for any injuries.');
 
 --usersToPrograms
 INSERT INTO usersToPrograms (username, program_id)
